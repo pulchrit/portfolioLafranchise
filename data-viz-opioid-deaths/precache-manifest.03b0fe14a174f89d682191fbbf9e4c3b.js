e7cf61aa5d6a30da9ab97a1a6b1bfcdd225b48a4
Melissa Lafranchise <pulchrit@gmail.com>
2019-11-24 12:57:26 -0800

    updated with data viz of opioid deaths

---
+++ data-viz-opioid-deaths/precache-manifest.03b0fe14a174f89d682191fbbf9e4c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8381305b304f67b743a1655399d5597",
+    "url": "/data-viz-opioid-deaths/index.html"
+  },
+  {
+    "revision": "8d56f4404402e297a541",
+    "url": "/data-viz-opioid-deaths/static/css/main.d329a8e0.chunk.css"
+  },
+  {
+    "revision": "266c23da4cabdc06887a",
+    "url": "/data-viz-opioid-deaths/static/js/2.9a8efeca.chunk.js"
+  },
+  {
+    "revision": "8d56f4404402e297a541",
+    "url": "/data-viz-opioid-deaths/static/js/main.b6de3386.chunk.js"
+  },
+  {
+    "revision": "8d23bb22bacd33afadea",
+    "url": "/data-viz-opioid-deaths/static/js/runtime-main.49888dc3.js"
+  },
+  {
+    "revision": "122c5bb16623928c7db51c74a0cfbab5",
+    "url": "/data-viz-opioid-deaths/static/media/GBD_2017_death_rate_opioid_use_disorders_all_ages.122c5bb1.csv"
+  }
+]);
