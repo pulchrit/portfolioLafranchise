bed6b015acf65dfeac248b48abc96d71adfe442c
Melissa Lafranchise <pulchrit@gmail.com>
2019-12-15 09:09:08 -0800

    added weirdness calculator

---
+++ weirdness/precache-manifest.9a655f12a4c59f86341418927a4fef9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ec1dc5a149748f2bb4e243ca7f0024",
+    "url": "/weirdness/index.html"
+  },
+  {
+    "revision": "17a6b8a93bd14b24d8c5",
+    "url": "/weirdness/static/css/main.319c7081.chunk.css"
+  },
+  {
+    "revision": "48398111dba3d120f461",
+    "url": "/weirdness/static/js/2.6ded5da2.chunk.js"
+  },
+  {
+    "revision": "17a6b8a93bd14b24d8c5",
+    "url": "/weirdness/static/js/main.464d82ed.chunk.js"
+  },
+  {
+    "revision": "c57e80fad41f338cc056",
+    "url": "/weirdness/static/js/runtime-main.73df77d5.js"
+  }
+]);
